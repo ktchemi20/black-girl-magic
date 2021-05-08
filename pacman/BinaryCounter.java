6090d19db4b05c6aaba0d293a274403fc437356f
Azyah Martin <aamartin20@students.desu.edu>
2021-05-07 22:35:29 -0400

    Added PowerPoint

--- pacman/BinaryCounter.java
+++ pacman/BinaryCounter.java
@@ -1,34 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.io.*;
 /**
- * Write a description of class BinaryCounter here.
+ * Write a description of class CounterBinary1 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class BinaryCounter extends Counter
+public class BinaryCounter extends Actor
 {
-    private String binaryScore = "";
+    public String binaryScore = "";
     /**
-     * Act - do whatever the BinaryCounter wants to do. This method is called whenever
+     * Act - do whatever the CounterBinary1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
         setImage(new GreenfootImage("Binary Score: " + binaryScore, 20,Color.WHITE, Color.BLACK));
-    }
-
-    public static void bAddScore(int score){
-        /*
+    }    
+    
+    public void bAddScore(int score){
+        
         while(score > 0){
             binaryScore = "" + (score % 2) + binaryScore;
             score /= 2;
         }
-        return binaryScore;
-        */ 
+        //return binaryScore;
+         
        
-        System.out.println(Integer.toBinaryString(score));
+        //System.out.println(Integer.toBinaryString(score));
     
     }
-
-}    
+}
